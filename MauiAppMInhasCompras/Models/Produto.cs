10542806a318c02b16a690205788cb724438b479
mahhtpx <maelianacleto19@gmail.com>
2026-03-20 20:19:07 -0300

    arrumamos o erro de duplicação e criamos uma restrição quando criar nocos produtos

--- MauiAppMInhasCompras/Models/Produto.cs
+++ MauiAppMInhasCompras/Models/Produto.cs
@@ -5,9 +5,22 @@ namespace MauiAppMinhasCompras.Models
 
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement] // que vem do SQLite
         public int Id { get; set; }
-        public string Descricao { get; set; }  
+        public string Descricao {
+            get => _descricao; //montamos uma restrição para não criae um produto sem descrição
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preenvha a descrção!");
+                }
+
+                _descricao = value;
+            }
+        }  
         public double Quantidade { get; set; }
         public double Preco { get; set; }
 
